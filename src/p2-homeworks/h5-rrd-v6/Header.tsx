fc9d05f41466904ea8ebdaa23aba2ed670665f47
hamitovm <hamitovm@gmail.com>
2022-05-17 23:58:58 +0300

    5 homework

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import mod from './Header.module.css'
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    return (
+        <div className={mod.navlinksBlockStyle}>
+            <NavLink className={mod.navlinkStyle} to="/pre-junior">Pre-junior</NavLink>
+            <NavLink className={mod.navlinkStyle} to="/junior">Junior</NavLink>
+            <NavLink className={mod.navlinkStyle} to="/junior-plus">Junior+</NavLink>
         </div>
     )
 }
